c40892adf4a72cb3cf3824ce31714bcc39ce49ba
Saklain Ahmad <saklainhmd@gmail.com>
2023-05-11 01:52:19 +0530

    Added props

--- src/Cart.js
+++ src/Cart.js
@@ -3,15 +3,56 @@ import CartItem from "./CartItem";
 
 
 class Cart extends React.Component {
+    
+    constructor () {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Mobile Phone',
+                    qty: 1,
+                    img: '',
+                    id:1,
+                },
+                {
+                    price: 99,
+                    title: 'Watch',
+                    qty: 1,
+                    img: '',
+                    id:2,
+                },
+                {
+                    price: 555,
+                    title: 'computer',
+                    qty: 1,
+                    img: '',
+                    id:3,
+                },
+                {
+                    price: 12000,
+                    title: 'Laptop',
+                    qty: 1,
+                    img: '',
+                    id:4,
+                }
+            ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
 
     render() {
-        const arr = [1,2,3,4,5];
-
+        const { products } = this.state;
         return (
             <div className="cart">
-                { arr.map( (item) => {
-                    return  item + 5;
-                }) }    
+                {products.map( (product) =>{
+                    return (
+                        <CartItem
+                            product= {product}
+                            key={product.id}
+                        />
+                    );
+                }) }
             </div>
             
         );
