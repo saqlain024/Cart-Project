1c47f2563d844783134e19ca6de2e221e3d60193
Saklain Ahmad <saklainhmd@gmail.com>
2023-05-11 12:18:01 +0530

    Adding product count to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import CartItem from "./CartItem";
 import Cart from "./Cart";
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Cart />
     </div>
   );
