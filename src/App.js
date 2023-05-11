ae4cbfa685630f54de49bb41a45e1c589b33ea17
Saklain Ahmad <saklainhmd@gmail.com>
2023-05-11 13:07:55 +0530

    Added total price

--- src/App.js
+++ src/App.js
@@ -15,28 +15,28 @@ class App extends React.Component {
                 price: 999,
                 title: 'Mobile Phone',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=80',
                 id:1,
             },
             {
                 price: 99,
                 title: 'Watch',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80',
                 id:2,
             },
             {
                 price: 555,
                 title: 'computer',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1586210579191-33b45e38fa2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
                 id:3,
             },
             {
                 price: 12000,
                 title: 'Laptop',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
                 id:4,
             }
         ]
@@ -100,6 +100,19 @@ class App extends React.Component {
     return count;
   }
 
+
+  getCartTotal = () => {
+      const  { products } = this.state;
+
+      let cartTotal = 0;
+
+      products.map( (product) => {
+        cartTotal = cartTotal + product.qty * product.price
+      })
+
+      return cartTotal;
+  }
+
   render() {
     const { products } = this.state;
     return (
@@ -113,6 +126,7 @@ class App extends React.Component {
           onDecreaseQuantity= { this.handleDecreaseQuantity }
           onDeleteProduct= { this.handleDeleteProduct }
         />
+        <div style= {{ padding:10, fontSize: 22 }}>TOTAL: { this.getCartTotal() } </div>
       </div>
     );
   }
