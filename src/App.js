4c284044389df3a19caf2abf29241f906b56bff6
Saklain Ahmad <saklainhmd@gmail.com>
2023-05-13 18:08:00 +0530

    read data from firebase v done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartItem from "./CartItem";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
 import React from "react";
+import firebase from "firebase";
 
 
 class App extends React.Component {
@@ -10,42 +11,42 @@ class App extends React.Component {
   constructor () {
     super();
     this.state = {
-        products: [
-            {
-                price: 999,
-                title: 'Mobile Phone',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=80',
-                id:1,
-            },
-            {
-                price: 99,
-                title: 'Watch',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80',
-                id:2,
-            },
-            {
-                price: 555,
-                title: 'computer',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1586210579191-33b45e38fa2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
-                id:3,
-            },
-            {
-                price: 12000,
-                title: 'Laptop',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-                id:4,
-            }
-        ]
+        products: [],
+        loading:true
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
 
+
+  componentDidMount () {
+    firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then( (snapshot) => {
+        console.log(snapshot);
+
+        snapshot.docs.map((doc) => {
+          console.log( doc.data() );
+        })
+      
+        const products = snapshot.docs.map( (doc) => {
+          const data = doc.data();
+
+          data['id'] = doc.id
+          return data;
+        })
+
+        this.setState({ 
+          products,
+          loading: false
+        })
+
+      })
+  }
+
   handleIncreaseQuantity = (product) => {
-      console.log('hey please inc the qty of ', product);
+      // console.log('hey please inc the qty of ', product);
       const { products } = this.state;
 
       const index = products.indexOf(product);
@@ -60,7 +61,7 @@ class App extends React.Component {
 
 
   handleDecreaseQuantity = (product) => {
-      console.log('hey please dec the qty of ', product);
+      // console.log('hey please dec the qty of ', product);
       const { products } = this.state;
 
       const index = products.indexOf(product);
@@ -103,7 +104,6 @@ class App extends React.Component {
 
   getCartTotal = () => {
       const  { products } = this.state;
-
       let cartTotal = 0;
 
       products.map( (product) => {
@@ -114,7 +114,7 @@ class App extends React.Component {
   }
 
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar 
@@ -127,6 +127,7 @@ class App extends React.Component {
           onDecreaseQuantity= { this.handleDecreaseQuantity }
           onDeleteProduct= { this.handleDeleteProduct }
         />
+        { loading &&  <h1>Loading product...</h1>} 
         <div  className="glass-effect-total" >TOTAL: { this.getCartTotal() } </div>
       </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//firebase
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf01NChS8t5cjO_cNIDJxRur65xESG7FU",
+  authDomain: "cart-bd2b2.firebaseapp.com",
+  projectId: "cart-bd2b2",
+  storageBucket: "cart-bd2b2.appspot.com",
+  messagingSenderId: "292458377680",
+  appId: "1:292458377680:web:4ade3cb4c49d0f8094c9d5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
